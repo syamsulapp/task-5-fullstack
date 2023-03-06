1f3fed45867bc22144a6393192c0afba0772d882
syamsulapp <samsul.marif100@gmail.com>
2023-03-06 15:48:34 +0800

    encrypt password login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -26,7 +27,7 @@ return new class extends Migration
         DB::table('users')->insert([
             'name' => 'administrator',
             'email' => 'administrator@gmail.com',
-            'password' => 'administrator',
+            'password' => Hash::make('administrator'),
         ]);
     }
 
