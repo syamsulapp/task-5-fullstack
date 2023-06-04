2eeeba6ed29a61a13b41f676b271ad0952dfe6c3
Muhammad Syamsul Marif F1B118038 <syamsulmar_f1b118038@student.uho.ac.id>
2023-06-04 16:41:33 +0800

    unit test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,6 +12,6 @@ class ExampleTest extends TestCase
     public function test_example()
     {
         $this->get('/')
-            ->assertSeeText('hei');
+            ->assertStatus(200);
     }
 }
